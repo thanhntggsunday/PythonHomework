9a8391dd39da7cd44c37d6e364907573f074f11f
thanhnt <thanhntggsunday@gmail.com>
2016-10-11 18:02:34 +0700

    Commit first of Bài 1.py

---
+++ BasicSyntax/Điều kiện if then else/Bài 1.py
@@ -0,0 +1,14 @@
+score = int(input('Score is: '))
+#score = 40
+if score >= 90:
+    print('Grade A')
+elif score >= 80:
+    print('Grade B')
+elif score >= 70:
+    print('Grade C')
+elif score >= 60:
+    print('Grade D')
+elif score >= 50:
+    print('Grade E')
+else:
+    print('Grade F')
